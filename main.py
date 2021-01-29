9aa680b308dfaecf52b9716194b8ea839826069a
repl.it user <replituser@example.com>
2021-01-29 02:58:04 +0000

    added project description

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
-print ("Python is really a great language,", "yeah?")
+# Author: Molly Johnson
+# Date: 1/27/21
+# Description:
+# Accepts an excel file of the previous month's CBOC sign in sheet,
+# extracts the year and ending day/date to adjust for the next calendar month,
+# and creates a new sheet accordingly. Adjusts for no CBOCs on weekends
+# and federal holidays.
+
