eff647e781ef63d59e3685185dea410b717d8dbf
Molly Johnson <johnsmol@oregonstate.edu>
2021-03-24 10:28:26 -0500

    added vscode settings and updated main.py to create basic excel file and sheet

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 
 
 # Author: Molly Johnson
 # Date: 3/24/21
-# Description:
+# Description: Creates cboc signin excel file for
+# checking in clinics for one month. Will adjust 
+# for the days of the month, weekends, and federal holidays
 
+# openpyxl imports
+from openpyxl import Workbook
+
+# create workbook and sheet
+wb = Workbook()
+ws = wb.active
+ws.title = "cboc_signin_sheet"
+ws.sheet_properties.tabColor = "1072BA"
+
+
+
+# save workbook to excel file
+wb.save('cboc_signin_sheet.xlsx')    
