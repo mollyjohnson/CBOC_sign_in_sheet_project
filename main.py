d69479ae3582a88446c4f8ee2e5487d6defdb6e8
Molly Johnson <johnsmol@oregonstate.edu>
2021-03-27 15:28:59 -0500

    removed test prints

--- main.py
+++ main.py
@@ -17,29 +17,21 @@ ws = wb.active
 ws.title = "cboc_signin_sheet"
 ws.sheet_properties.tabColor = "1072BA"
 
-# TEST_START
-
 # get start date of the month from user
-startDateStr = input("Enter month's start date in the format mm-dd-yyyy: ")
-print('your start date string is: ', startDateStr)
+startDateStr = input("\nEnter month's start date in the format mm-dd-yyyy: ")
 
 # Create date object in format mm-dd-yyyy
 startDateObj = datetime.strptime(startDateStr, "%m-%d-%Y")
 
-print(startDateObj)
-print('Type: ',type(startDateObj))
-
-print('Day of Month', startDateObj.day)
-
+#print(startDateObj)
+#print('Type: ',type(startDateObj))
+print('\nDay of Month', startDateObj.day)
 #to get name of day (in number) from date
 print('Day of Week (number): ', startDateObj.weekday())
-
 # to get name of day from date
 print('Day of Week (name): ', calendar.day_abbr[startDateObj.weekday()])
-
 # to get name of month from date
 print('Month name: ', calendar.month_name[startDateObj.month])
-#TEST_END
 
 # save workbook to excel file
 wb.save('cboc_signin_sheet.xlsx')   
