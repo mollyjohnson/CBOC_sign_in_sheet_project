3d8a240157c0c86a0dbd396401dc5ffd46a4fc51
Molly Johnson <johnsmol@oregonstate.edu>
2022-06-05 15:47:40 -0500

    read in CBOC names from .txt file

--- main.py
+++ main.py
@@ -6,6 +6,14 @@
 # checking in clinics for every month in one year. Will adjust 
 # for the days of the month, weekends, and federal holidays
 
+# function comment template
+####################################################################
+### Function Title:
+### Arguments:
+### Returns:
+### Description: 
+###################################################################
+
 # import necessary libraries 
 from openpyxl import Workbook
 from datetime import datetime
@@ -17,3 +25,41 @@ import federalHolidayCalculator
 import CBOC
 
 # create "constants"
+# CBOC file name
+CBOC = 'CBOCs.txt'
+
+####################################################################
+### Function Title:
+### Arguments:
+### Returns:
+### Description: 
+###################################################################
+# Check whether the CBOC needs an SMP check line or not
+
+
+####################################################################
+### Function Title:
+### Arguments:
+### Returns:
+### Description: 
+###################################################################
+# open CBOC.txt file
+file = open(CBOC,'r')
+
+# use readlines to read all lines in the text file and
+# return the file contents as a list of strings
+lines = []
+lines = file.readlines()
+
+# count num CBOCs from the file
+numCBOCs = 0
+
+# go through each line and strip the added newline character and increment number of CBOCs
+for line in lines:
+    print(line.rstrip('\n'))
+    numCBOCs += 1
+
+print('\n' + str(numCBOCs))
+
+# close .txt file
+file.close()
