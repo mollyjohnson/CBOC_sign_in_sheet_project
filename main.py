ce045777dc2e469e0a0599213859658cb6dd382d
Molly Johnson <johnsmol@oregonstate.edu>
2022-06-05 17:43:38 -0500

    smp and non smp cboc names separated into different lists

--- main.py
+++ main.py
@@ -27,19 +27,34 @@ import readFile
 
 # create "constants"
 
-####################################################################
-### Function Title:
-### Arguments:
-### Returns:
-### Description: 
-###################################################################
-# create lists for both SMP and non SMP CBOC names
-smpCBOCs = []
-noSMPCBOCs = []
 
-# get CBOCs from file
-smpCBOCs, noSMPCBOCs = readFile.getCBOClists(smpCBOCs, noSMPCBOCs)
-print ('SMP CBOCs:')
-print(smpCBOCs)
-print('no SMP CBOCs:')
-print(noSMPCBOCs)
+####################################################################
+### Function Title: main()
+### Arguments: none
+### Returns: none
+### Description: has loop for each month of the given year chosen by
+### the user. creates workbook and 2 worksheets per month. calls other
+### functions used to get user info, get the datetime object, calculate
+### the num of days in the month, calculate the federal holidays, set
+### all row and column info for all cells in the cboc signin sheet,
+### creates a folder for each year's worksheets (unless the folder
+### already exists), and saves all excel spreadsheets using the month
+### number/name formatted so that it will have them in correct month
+### order in the folder (jan - dec for the given year).
+###################################################################
+def main():
+    # create lists for both SMP and non SMP CBOC names
+    smpCBOCs = []
+    noSMPCBOCs = []
+
+    # get CBOCs from file
+    smpCBOCs, noSMPCBOCs = readFile.getCBOClists(smpCBOCs, noSMPCBOCs)
+
+    for cboc in noSMPCBOCs:
+        print(cboc)
+
+    for cboc in smpCBOCs:
+        print(cboc)
+
+if __name__ == "__main__":
+    main()
